f534307088050f1c005304806d1b2ee334663226
Alexander Pierola <alexdevelopit@gmail.com>
2020-04-17 00:45:29 +0200

    nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,6 +3,27 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/qpl_logo.svg';
 
 const Nav = () => {
+    document.addEventListener('DOMContentLoaded', () => {
+        // Get all "navbar-burger" elements
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+            // Add a click event on each of them
+            $navbarBurgers.forEach(el => {
+                el.addEventListener('click', () => {
+                    // Get the target from the "data-target" attribute
+                    const target = el.dataset.target;
+                    const $target = document.getElementById(target);
+                    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                    el.classList.toggle('is-active');
+                    $target.classList.toggle('is-active');
+
+                });
+            });
+        }
+
+    });
+
     return (
         <div className="container is-fluid">
             <nav class="navbar" role="navigation" aria-label="main navigation">
