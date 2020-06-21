228d70761b5c1396b4ef0595893f7a33bb0df80d
Alexander <alexdevelopit@gmail.com>
2020-06-21 20:06:00 +0200

    build

---
+++ build/precache-manifest.914bad0bf5ba5d0754b192b89684cc9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e6de707b25811cdb3641af076b3756",
+    "url": "/quickplantuml/index.html"
+  },
+  {
+    "revision": "b259369fdbb09d41e1c1",
+    "url": "/quickplantuml/static/css/2.a2c18f5e.chunk.css"
+  },
+  {
+    "revision": "984bbf3d5291e7f78d2b",
+    "url": "/quickplantuml/static/css/main.3419ee8f.chunk.css"
+  },
+  {
+    "revision": "b259369fdbb09d41e1c1",
+    "url": "/quickplantuml/static/js/2.fe6e7209.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quickplantuml/static/js/2.fe6e7209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984bbf3d5291e7f78d2b",
+    "url": "/quickplantuml/static/js/main.58c238af.chunk.js"
+  },
+  {
+    "revision": "46f76ec139ebcfaf5dc1",
+    "url": "/quickplantuml/static/js/runtime-main.e6ee3334.js"
+  },
+  {
+    "revision": "b2ebc6b92d574d98c29cc0cfd0afc5ce",
+    "url": "/quickplantuml/static/media/qpl_logo.b2ebc6b9.svg"
+  }
+]);
